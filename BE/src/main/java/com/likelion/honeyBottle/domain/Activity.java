23d2a9d9fcbc96b09175cd2e388e7e34e4bc73bd
178kg78cm <jhh7550@gmail.com>
2023-08-18 22:26:20 +0900

    third
    custum

--- BE/src/main/java/com/likelion/honeyBottle/domain/Activity.java
+++ BE/src/main/java/com/likelion/honeyBottle/domain/Activity.java
@@ -1,12 +1,8 @@
 package com.likelion.honeyBottle.domain;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
 public class Activity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
