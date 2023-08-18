8707e9ee778f7d08dc07d6f7d21cb8155fc8fbe0
178kg78cm <jhh7550@gmail.com>
2023-08-18 23:10:21 +0900

    forth
    modi

--- BE/src/main/java/com/likelion/honeyBottle/HoneyBottleApplication.java
+++ BE/src/main/java/com/likelion/honeyBottle/HoneyBottleApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HoneyBottleApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HoneyBottleApplication.class, args);
 	}
